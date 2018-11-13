7657509bfa7ef4b22a341e135de79077fd1905f9
ohrytsiuk <greem92@gmail.com>
2018-11-13 14:09:03 +0200

    First implementation

---
+++ calculation/calculation.py
@@ -0,0 +1,11 @@
+"""Functions for test"""
+
+
+def add(num1, num2):
+    """add two numbers"""
+    return num1 + num2
+
+
+def divide(num1, num2):
+    """divide number1 on number2"""
+    return num1 / num2
