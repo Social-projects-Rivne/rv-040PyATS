63bbc38093fc89da156915dcad56f03a7c98ab88
ohrytsiuk <greem92@gmail.com>
2018-12-14 15:07:48 +0200

    another try

--- cisco/test.py
+++ cisco/test.py
@@ -9,7 +9,7 @@ from genie.conf import Genie
 from genie.conf.base.config import CliConfig
 from pyats.topology import loader
 from genie.libs import ops
-from genie.libs.ops.interface.nxos.interface import Interface
+from genie.libs.ops.interface.iosxe.interface import Interface
 
 PROJECT_DIR = os.path.dirname(__file__)
 
@@ -18,19 +18,19 @@ class UnitTest(aetest.Testcase):
     """Class for testing"""
 
     @aetest.setup
-    def setup(self):
+    def setup(self, testbed):
         """Set up"""
-        self.testbed = Genie.init('testbed.yaml')
-        # self.vm1 = self.testbed.devices['Router1']
-        # self.vm1.connect(via='a')
+        self.testbed = Genie.init(testbed)
+        self.vm1 = self.testbed.devices['c3745']
+        self.vm1.connect(via='a')
         # with open('c3745_startup-config.cfg', 'r') as file:
         #     config = file.read()
         #     self.vm1.configure(config)
         #     # CliConfig(device=self.vm1, unconfig=False, cli_config=config)
         #     # self.vm1.build_config()
-        #
-        # self.vm2 = self.testbed.devices['Router2']
-        # self.vm2.connect(via='a')
+
+        self.vm2 = self.testbed.devices['c3725']
+        self.vm2.connect(via='a')
         # with open('c3725_startup-config.cfg', 'r') as file:
         #     config = file.read()
         #     # CliConfig(device=self.vm2, unconfig=True, cli_config=config)
@@ -40,28 +40,55 @@ class UnitTest(aetest.Testcase):
 
     @aetest.test
     def test(self):
-        # print(self.vm1.ping('192.168.1.1'))
-        print("olala")
 
-        for name, device in self.testbed.devices.items():
+        interfaces = Interface(device=self.vm1)
+        interfaces.learn()
+        # pprint(intf1.info)
 
-            device.connect()
-            abstract = Lookup.from_device(device)
-            intf = abstract.ops.interface.interface.Interface(device)
-            intf.learn()
-            pprint(intf.info)
+        for interface, value in interfaces.info.items():
+            if interface in self.vm1.interfaces:
+                pass
 
-            for str_intf in intf.info:
-                if intf.info[str_intf].get('oper_status', None) and intf.info[str_intf]['oper_status'] == 'up':
-                    assert True
-                else:
-                    assert False
+        links = self.vm1.find_links(self.vm2)
+
+        # abstract1 = Lookup.from_device(self.vm1)
+        # intf1 = abstract1.ops.interface.interface.Interface(self.vm1)
+        # intf1.learn()
+        # pprint(intf1.info)
+
+        # with open('c3745', 'wb') as f:
+        #     f.write(intf1.pickle())
+
+        # abstract2 = Lookup.from_device(self.vm2)
+        # intf2 = abstract2.ops.interface.interface.Interface(self.vm2)
+        # intf2.learn()
+        # pprint(intf2.info)
+        # # with open('c3725', 'wb') as f:
+        # #     f.write(intf2.pickle())
+
+        # result1 = self.vm1.execute('show version')
+
+        # # print(self.vm1.ping('192.168.1.1'))
+        # print("olala")
+        #
+        # for name, device in self.testbed.devices.items():
+        #
+        #     device.connect()
+        #     abstract = Lookup.from_device(device)
+        #     intf = abstract.ops.interface.interface.Interface(device)
+        #     intf.learn()
+        #     pprint(intf.info)
+        #
+        #     for str_intf in intf.info:
+        #         if intf.info[str_intf].get('oper_status', None) and intf.info[str_intf]['oper_status'] == 'up':
+        #             assert True
+        #         else:
+        #             assert False
 
     @aetest.cleanup
     def cleanup(self, testbed):
-        # self.vm1.disconnect()
-        # self.vm2.disconnect()
-        pass
+        self.vm1.disconnect()
+        self.vm2.disconnect()
 
 
 if __name__ == '__main__':
